ec94777021bf52c3ebe8b2caab9b9c3379054252
Taranjit Kaur <taranjitcaur@gmail.com>
2020-10-08 17:46:15 -0400

    Accordion using useState hook

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,30 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class Accordion extends React.Component {
-       state = {indexClicked:''}
-      onItemClicked = (index) => {
-        this.setState({indexClicked: index});
-      }
-      renderedItems =  this.props.items.map((item,index) => {
-        return (<React.Fragment key={item.title}>
-        <div className="title active" onClick={() => this.onItemClicked(index)}>
-            <i className="dropdown icon"></i>
+const Accordion = ({items}) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+   const updateStateItem = (index) => {
+        setActiveIndex(index);
+   }
+   const renderedItems =  items.map((item, index) => {
+    const active = (index === activeIndex) ? 'active' : '';
+      return(<React.Fragment key={index}>         
+        <div className= {`title ${active}`} onClick = {() => updateStateItem(index)}>
+          <i className="dropdown icon"></i>
             {item.title}
         </div>
-        <div className="content active">
-            <p>{item.content}</p>
+        <div className={`content ${active}`}>
+        {item.content}
         </div>
-        </React.Fragment>  
-        )
-      }) 
-    
-    render() {
-       return(
-         <div>
-          {this.state.indexClicked }
-         {this.renderedItems}
-         </div>         
-         );
-      }
-  }
+      </React.Fragment>
+      )
+    }
+  )
+ return <div className="ui styled accordion">{renderedItems}</div>;
+}
 export default Accordion;
