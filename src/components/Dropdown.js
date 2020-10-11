72ef717ead47aff5548ed58d90cbcb045c2340bf
Taranjit Kaur <taranjitcaur@gmail.com>
2020-10-11 14:47:46 -0400

    update useRef and useEffect

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 const Dropdown = ({listContent}) => {
     const [selectedValue, setSelectedValue] = useState("");
     const [isSelected, setSelected] = useState(false);
     const [isChildSelected, setChildSelected] = useState(false);
+    const ref = useRef();
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+          if (ref.current.contains(event.target)) {
+            return;
+          }    
+          setChildSelected(false);
+        });
+      }, []);
     
     const parentElementClicked = (event) => {
         setSelected(true);
@@ -16,7 +25,7 @@ const Dropdown = ({listContent}) => {
     return(<div className="item" onClick={(e)=> childElementClicked(e, item.value)} key={item.value}>{item.value}</div>)})
        return(        
         <React.Fragment>     
-            <div className="eight wide column"> 
+            <div className="eight wide column" ref={ref}> 
             <div className="eight wide column"><label>Select a color</label></div>         
             <div className={`ui selection dropdown error ${(isSelected ? "active visible" : !isSelected)}`} 
             onClick = {(e) => parentElementClicked(e)}>            
